a295bf2cb79d9495911422bd01b9e5c3aaef33e9
Weiming <zhuangweiming@sensetime.com>
2021-05-20 20:54:33 +0800

    feat[script]: update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-mkdir -p log
-now=$(date +"%Y%m%d_%H%M%S")
-
-data_dir=/mnt/lustre/$(whoami)/projects/easyfl/easyfl/datasets/data
-root_dir=/mnt/lustre/$(whoami)/projects/leaf
-
-export PYTHONPATH=$PYTHONPATH:${root_dir}
-
-srun -u --partition=innova --job-name=leaf-femnist \
-    -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python ${root_dir}/models/main.py --dataset femnist --model cnn \
-    --data_dir ${data_dir}/femnist/femnist_iid_100_10_1_0.05_0.1_sample_0.9/ \
-    --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 5 --lr 0.01 | tee log/leaf-femnist-${now}.log &
+python ~/personal-projects/leaf/models/main.py --dataset femnist --model cnn --eval-every 1 \
+    --data_dir ~/personal-projects/easyfl/easyfl/datasets/data/femnist/femnist_iid_100_10_1_0.05_0.1_sample_0.9/ \
+    --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 5 --lr 0.01
