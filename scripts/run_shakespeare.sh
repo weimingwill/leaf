5f1a939c00d35039c2e78f0be608e0c205eedeb0
Weiming <wingalong@gmail.com>
2021-05-26 20:20:08 +0800

    update script

--- scripts/run_shakespeare.sh
+++ scripts/run_shakespeare.sh
@@ -3,13 +3,11 @@
 mkdir -p log
 now=$(date +"%Y%m%d_%H%M%S")
 
-data_dir=/mnt/lustre/$(whoami)/projects/easyfl/easyfl/datasets/data
-root_dir=/mnt/lustre/$(whoami)/projects/leaf
+data_dir=~/personal-projects/easyfl/easyfl/datasets/data
+root_dir=~/personal-projects/leaf
 
 export PYTHONPATH=$PYTHONPATH:${root_dir}
 
-srun -u --partition=innova --job-name=leaf-shakespeare \
-    -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python ${root_dir}/models/main.py --dataset shakespeare --model stacked_lstm --eval-every 1 \
+python ${root_dir}/models/main.py --dataset shakespeare --model stacked_lstm --eval-every 1 \
     --data_dir ${data_dir}/shakespeare/shakespeare_iid_100_10_1_0.05_0.1_sample_0.9/ \
-    --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 5 --lr 0.01 | tee log/leaf-shakespeare-${now}.log &
+    --clients-per-round 10 --num-rounds 100 --batch-size 64 --num-epochs 10 --lr 0.01 | tee log/leaf-shakespear-${now}.log
