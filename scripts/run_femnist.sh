62800634d068b607d0dcbb7f7a32ec6c56764ea9
Weiming <wingalong@gmail.com>
2021-06-02 15:26:35 +0800

    update script

--- scripts/run_femnist.sh
+++ scripts/run_femnist.sh
@@ -10,5 +10,5 @@ export PYTHONPATH=$PYTHONPATH:${root_dir}
 
 python -u ${root_dir}/models/main.py --dataset femnist --model cnn --eval-every 1 \
     --data_dir ${data_dir}/femnist/femnist_iid_10_10_1_0.05_0.05_sample_0.9/ \
-    --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 10 --lr 0.01 --test-all | tee log/leaf-femnist-${now}.log
+    --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 10 --lr 0.01 --test-all | tee log/leaf-femnist-${now}.log &
 
--- scripts/run_femnist_cluster.sh
+++ scripts/run_femnist_cluster.sh
@@ -12,5 +12,5 @@ srun -u --partition=innova --job-name=leaf-femnist \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
     python -u ${root_dir}/models/main.py --dataset femnist --model cnn --eval-every 1 \
       --data_dir ${data_dir}/femnist/femnist_iid_10_10_1_0.05_0.05_sample_0.9/ \
-      --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 10 --lr 0.01 --test-all | tee log/leaf-femnist-${now}.log
+      --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 10 --lr 0.01 --test-all | tee log/leaf-femnist-${now}.log &
 
