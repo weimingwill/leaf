062c9d9fe22d843bcc21eb860a03c716b7d26fc3
Weiming <wingalong@gmail.com>
2021-06-04 14:51:45 +0800

    update script

--- scripts/run_shakespeare.sh
+++ scripts/run_shakespeare.sh
@@ -10,4 +10,4 @@ export PYTHONPATH=$PYTHONPATH:${root_dir}
 
 python -u ${root_dir}/models/main.py --dataset shakespeare --model stacked_lstm --eval-every 1 \
     --data_dir ${data_dir}/shakespeare/shakespeare_iid_10_10_1_0.2_0.2_sample_0.9/ \
-    --clients-per-round 10 --num-rounds 100 --batch-size 64 --num-epochs 10 --lr 0.8 | tee log/leaf-shakespear-${now}.log &
+    --clients-per-round 10 --num-rounds 100 --batch-size 64 --num-epochs 10 --lr 0.8 --test-all | tee log/leaf-shakespear-${now}.log &
