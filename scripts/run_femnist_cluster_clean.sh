8dddd1e08166060073d3479a480ecaa3da907cf0
Weiming <wingalong@gmail.com>
2022-07-22 17:47:51 +0800

    add scripts

--- scripts/run_femnist_cluster_clean.sh
+++ scripts/run_femnist_cluster_clean.sh
@@ -7,10 +7,13 @@ data_dir=/mnt/lustre/$(whoami)/projects/easyfl/easyfl/datasets/data
 root_dir=/mnt/lustre/$(whoami)/projects/leaf
 
 export PYTHONPATH=$PYTHONPATH:${root_dir}
+export PATH="/mnt/lustre/share/cuda-10.1/bin:$PATH"
+export LD_LIBRARY_PATH="/mnt/lustre/share/cuda-10.1/lib64:$LD_LIBRARY_PATH"
+export CUDA_VISIBLE_DEVICES=0
 
 srun -u --partition=innova --job-name=leaf-femnist \
-    -n1 --gres=gpu:1 --ntasks-per-node=1 \
+    -n8 --gres=gpu:8 --ntasks-per-node=8 -w SG-IDC2-10-51-3-50 \
     python -u ${root_dir}/models/main.py --dataset femnist --model cnn --eval-every 1 \
       --data_dir ${data_dir}/femnist/femnist_iid_10_10_1_0.05_0.05_sample_0.9/ \
-      --clients-per-round 10 --num-rounds 150 --batch-size 64 --num-epochs 10 --lr 0.01 --test-all | tee log/leaf-femnist-${now}.log &
+      --clients-per-round 10 --num-rounds 100 --batch-size 64 --num-epochs 10 --lr 0.01 --test-all | tee log/leaf-femnist-${now}.log &
 
--- scripts/run_shakespeare_cluster_small.sh
+++ scripts/run_shakespeare_cluster_small.sh
@@ -12,4 +12,4 @@ srun -u --partition=innova --job-name=leaf-shakespeare \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
     python -u ${root_dir}/models/main.py --dataset shakespeare --model stacked_lstm --eval-every 1 \
       --data_dir ${data_dir}/shakespeare/shakespeare_iid_100_10_1_0.05_0.1_sample_0.9/ \
-      --clients-per-round 10 --num-rounds 100 --batch-size 64 --num-epochs 10 --lr 0.8 --test-all | tee log/leaf-shakespear-small-${now}.log &
+      --clients-per-round 10 --num-rounds 20 --batch-size 64 --num-epochs 10 --lr 0.8 --test-all | tee log/leaf-shakespear-small-20-${now}.log &
